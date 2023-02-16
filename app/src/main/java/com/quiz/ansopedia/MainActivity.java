52ab9f5bc8b9561e63bbffd01fd816a725f7b9fd
Bibek Kumar Sah <bkumarsah627@rku.ac.in>
2023-02-16 18:03:58 +0530

    Splash Screen Added

---
+++ app/src/main/java/com/quiz/ansopedia/MainActivity.java
@@ -0,0 +1,18 @@
+package com.quiz.ansopedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+//        splash screen start
+            SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+//        splash screen end
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
