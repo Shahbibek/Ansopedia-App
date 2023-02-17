dc9f3a82fbd6dd4ce7f8aa67a90a9e32ac1a0fa3
Bibek Kumar Sah <bkumarsah627@rku.ac.in>
2023-02-17 14:35:50 +0530

    signin page and portrait mode set

--- app/src/main/java/com/quiz/ansopedia/MainActivity.java
+++ app/src/main/java/com/quiz/ansopedia/MainActivity.java
@@ -3,17 +3,18 @@ package com.quiz.ansopedia;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        splash screen start
-            SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
-//        splash screen end
+//        ########################### splash screen start #############################################
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+//        ########################### splash screen End #############################################
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
+
 }
